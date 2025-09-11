30c00376aabb5d7c3696e3298843503015cb2d07
Gudisa1 <gudisagebi1@gmail.com>
2025-09-12 00:33:16 +0300

    Swagger Update

--- strategybackend/swagger_urls.py
+++ strategybackend/swagger_urls.py
@@ -18,9 +18,9 @@ schema_view = get_schema_view(
 )
 
 swagger_urls = [
-    path("swagger.json/", schema_view.without_ui(cache_timeout=0), name="schema-json"),
     path(
         "swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="swagger-ui"
     ),
+    path("swagger.json/", schema_view.without_ui(cache_timeout=0), name="schema-json"),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="redoc"),
 ]
--- strategybackend/urls.py
+++ strategybackend/urls.py
@@ -2,6 +2,8 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.contrib import admin
 from django.urls import path, include
 from strategybackend.swagger_urls import swagger_urls
+from django.views.generic import RedirectView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -9,6 +11,14 @@ urlpatterns = [
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/", include("users.urls")),
     path("api/", include("partners.urls")),
+    # Add authentication URLs to fix the 404 error
+    path(
+        "accounts/login/",
+        auth_views.LoginView.as_view(template_name="admin/login.html"),
+        name="login",
+    ),
+    path("accounts/logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("", RedirectView.as_view(url="/swagger/", permanent=True)),
 ]
 
 urlpatterns += swagger_urls
